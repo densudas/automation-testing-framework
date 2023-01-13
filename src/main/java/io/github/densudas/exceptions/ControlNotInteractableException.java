e54e41010de07fe665ad1fa07abe594073e083d4
Denis Sudas <dvsudas@gmail.com>
2023-01-13 17:37:33 +0100

    update exceptions

--- src/main/java/io/github/densudas/exceptions/ControlNotInteractableException.java
+++ src/main/java/io/github/densudas/exceptions/ControlNotInteractableException.java
@@ -1,22 +1,20 @@
 package io.github.densudas.exceptions;
 
 import io.github.densudas.controls.BaseControl;
+import java.util.Optional;
 import org.openqa.selenium.ElementNotInteractableException;
 
 //TODO: Update Errors mechanism
 public class ControlNotInteractableException extends RuntimeException {
   public ControlNotInteractableException(BaseControl control) {
-    var message =
-        new StringBuilder()
-            .append(control.getControlType().getName())
-            .append(" with name '")
-            .append(control.getName())
-            .append("' is not interactable.");
+    var message = new StringBuilder()
+        .append(control.getControlType().getName())
+        .append(" with name '")
+        .append(control.getName())
+        .append("' is not interactable.");
 
-    if (control.getError() != null) {
-      throw new ElementNotInteractableException(message.toString(), control.getError());
-    } else {
-      throw new ElementNotInteractableException(message.toString());
-    }
+    throw Optional.ofNullable(control.getError())
+        .map(e -> new ElementNotInteractableException(message.toString(), control.getError()))
+        .orElse(new ElementNotInteractableException(message.toString()));
   }
 }
